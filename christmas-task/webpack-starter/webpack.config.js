e20e3ade0491dcb4e0899e101f0c0abdffc51760
Valeria Karimova <valeriakarimova2912@gmail.com>
2021-12-08 16:50:23 +0300

    feat: create webpack for project

---
+++ christmas-task/webpack-starter/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    devtool: 'inline-source-map',
+    mode: 'development',
+
+    devServer: {
+        static: './dist',
+        // historyApiFallback: true,
+        // static: {
+        //     directory: path.join(__dirname, 'dist'),
+        //   },
+        // open: true,
+        // compress: true,
+        // hot: true,
+        // port: 8080,
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.bundle.js',
+        assetModuleFilename: 'assets/images/[name][ext]'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), 
+            filename: 'index.html', 
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist/**/*')]
+          }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['ts-loader'],
+            },
+
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+                // use: [
+                    // {
+                        // loader: 'file-loader',
+                    // },
+                // ],
+                
+            },
+
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+}
+
