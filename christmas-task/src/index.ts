73f4b7d59ca30d898a544703338080db2d167c5b
Valeria Karimova <valeriakarimova2912@gmail.com>
2021-12-13 15:13:47 +0300

    refactor: replace data.js by data.json

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,7 +1,10 @@
 import './assets/styles/index.css';
 import './assets/styles/toys.css';
 import './slider.js';
+import Application from '../ts/Application';
 import * as _ from 'lodash';
+
+const app = new Application();
 
 
 
